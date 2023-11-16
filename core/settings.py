8eff2548b3988d464ed879fe6782e8b3a900cce2
Jtrigo-corp <jtrigo.corp@gmail.com>
2023-11-16 13:31:28 -0300

    cambio en settings.py admin

--- core/settings.py
+++ core/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'droniadb',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': 'admin ',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
