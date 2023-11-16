5eac5feb90960b55e2984d3ee00168bd57a5f353
Jtrigo-corp <jtrigo.corp@gmail.com>
2023-11-16 13:47:14 -0300

    cambio en settings.py no pass test

--- core/settings.py
+++ core/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'droniadb',
-        'USER': 'admin',
+        'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
