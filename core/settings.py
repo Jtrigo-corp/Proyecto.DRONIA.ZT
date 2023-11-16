3efeec717c678ce70d4b394a02ca6fb6a403b4fa
Jtrigo-corp <jtrigo.corp@gmail.com>
2023-11-16 15:14:52 -0300

    cambio de ALLOWED_HOST para acceder desde la instancia prueba 2

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ DEBUG = 'RENDER' not in os.environ
 
 # HOSTs List
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['3.18.104.70','3.18.104.70:8000', 'localhost', '127.0.0.1']
 
 
 # Add here your deployment HOSTS
