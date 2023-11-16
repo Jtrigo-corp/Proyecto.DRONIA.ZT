06aed36d0587572919cace2aa363bd4b2f335ea7
Jtrigo-corp <jtrigo.corp@gmail.com>
2023-11-16 15:05:07 -0300

    cambio de ALLOWED_HOST para acceder desde la instancia 2

--- core/settings.py
+++ core/settings.py
@@ -21,10 +21,12 @@ if not SECRET_KEY:
 DEBUG = 'RENDER' not in os.environ
 
 # HOSTs List
-ALLOWED_HOSTS = ['3.18.104.70']
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['3.18.104.70', 'localhost', '127.0.0.1']
+
 
 # Add here your deployment HOSTS
-CSRF_TRUSTED_ORIGINS = ['http://3.18.104.70:8000','http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085']
+CSRF_TRUSTED_ORIGINS = ['http://3.18.104.70:8000']
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
