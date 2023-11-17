4a7fe11a28a6ac0d230d89784f7eac96eed4cabd
Jtrigo-corp <jtrigo.corp@gmail.com>
2023-11-17 11:41:50 -0300

    cambio de ALLOWED_HOST 02

--- core/settings.py
+++ core/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         
     }
