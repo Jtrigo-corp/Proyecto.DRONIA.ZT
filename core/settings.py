31a5f0b488f150fff08057952111e477b41fd5eb
Jtrigo-corp <jtrigo.corp@gmail.com>
2023-11-17 11:44:49 -0300

    cambio de ALLOWED_HOST 03

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ import django_dyn_dt
 load_dotenv()  # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
